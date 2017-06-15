29802664b6486896286ade03314c24aa0d9b524a
Shanmugavel Arunachalam <iashanmugavel@gmail.com>
2017-06-16 00:45:54 +0530

    added cap deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '35.154.39.200', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
